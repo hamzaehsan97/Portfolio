04ac0e6f7d93a75d1e14878c77c42c1e3e764dda
hamzaehsan97 <hamza.ehsan@lawrence.edu>
2020-07-02 16:05:17 -0500

    New projects

--- src/components/layout/ProjectRow.js
+++ src/components/layout/ProjectRow.js
@@ -22,38 +22,131 @@ export const ProjectRow = (props) => {
         className="App"
         justify="flex-end"
         alignItems="flex-start"
-        direction="row"
+        direction="column"
         spacing={6}
       >
         <Grid item>
-          <ProjectCard
-            project={{
-              name: "Dupolicity",
-              description:
-                "Dupolicity is an open source strategy cross-platform phone game built in Unity2D.",
-              url: "https://github.com/hamzaehsan97/dupolicity_Unity2D",
-            }}
-          />
+          <Grid
+            container
+            className="App"
+            justify="flex-end"
+            alignItems="flex-start"
+            direction="row"
+            spacing={6}
+          >
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "DroneSeek",
+                  description:
+                    "Computer Vision based autonomous drone (Tello). Programed in python and python based computer vision/ machine learning libraries.",
+                  url: "https://github.com/hamzaehsan97/AI_Rescue_Drone",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "Speech Recognition CNN",
+                  description:
+                    "This project is a speech recognition application built using Convolutional Neural Networks in Python. The project was integrated to a VR language game, VRnacular.",
+                  url: "https://github.com/hamzaehsan97/Dupolicity",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "Hotline",
+                  description:
+                    "Hotline is a React based real estate platform focused to the needs of south asian real estate markets. The project was built using React, Node.JS, Firebase and MongoDB.",
+                  url: "https://github.com/hamzaehsan97/Hotline",
+                }}
+              />
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item>
-          <ProjectCard
-            project={{
-              name: "DroneSeek",
-              description:
-                "Computer Vision based disaster relief drone (Tello) . This was my capstone project.",
-              url: "https://github.com/hamzaehsan97/AI_Rescue_Drone",
-            }}
-          />
+          <Grid
+            container
+            className="App"
+            justify="flex-end"
+            alignItems="flex-start"
+            direction="row"
+            spacing={6}
+          >
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "FlashPass",
+                  description:
+                    "Flash Pass is a cross platform car wash automation app written in Flutter. Flash pass provides a universal car wash pass based on geo-location through the phone.",
+                  url: "https://github.com/hamzaehsan97/car_wash",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "EV2EV",
+                  description:
+                    "EV2EV is an IOS app which was the winning project for MakeHarvard 2019. The app is an innovative solution to fixing America's EV charging grid issues.",
+                  url: "https://github.com/hamzaehsan97/MakeHarvard2019-Ev2Ev",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "Dupolicity",
+                  description:
+                    "Dupolicity is an open source strategy cross-platform phone game built in Unity2D.",
+                  url: "https://github.com/hamzaehsan97/Dupolicity",
+                }}
+              />
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item>
-          <ProjectCard
-            project={{
-              name: "Portfolio",
-              description:
-                "This Project contains code to the Portfolio you are currently viewing.",
-              url: "https://github.com/hamzaehsan97/Portfolio",
-            }}
-          />
+          <Grid
+            container
+            className="App"
+            justify="flex-end"
+            alignItems="flex-start"
+            direction="row"
+            spacing={6}
+          >
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "Portfolio",
+                  description:
+                    "This Project contains code to the Portfolio you are currently viewing.",
+                  url: "https://github.com/hamzaehsan97/Portfolio",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "Virus Growth Patterns",
+                  description:
+                    "V.G.P. is a data science based project coded in R. The project predicted the growth of COVID-19 virus based on travel patterns.",
+                  url: "https://github.com/hamzaehsan97/VirusGrowthPatterns",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <ProjectCard
+                project={{
+                  name: "College Rentention Rates",
+                  description:
+                    "C.R.R. is a data science project that studies the relationship between student's financial well-being and student dropout rates.",
+                  url: "https://github.com/hamzaehsan97/CollegeRetentionRates",
+                }}
+              />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
