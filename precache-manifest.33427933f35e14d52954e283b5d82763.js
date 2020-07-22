7d4da94de62e95a75daa71bd5a4a7b9e79c06205
hamzaehsan97 <hamza.ehsan@lawrence.edu>
2020-07-21 21:11:08 -0500

    Updates

---
+++ precache-manifest.33427933f35e14d52954e283b5d82763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a25a496fcb6e41b23328915cf31823",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ebad17f15e0855cc454b",
+    "url": "/Portfolio/static/css/main.14fd9768.chunk.css"
+  },
+  {
+    "revision": "c029e9b0b4b55a55c2d8",
+    "url": "/Portfolio/static/js/2.b828c792.chunk.js"
+  },
+  {
+    "revision": "6734d0bfe367c40a5b2a23ab2f00b933",
+    "url": "/Portfolio/static/js/2.b828c792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebad17f15e0855cc454b",
+    "url": "/Portfolio/static/js/main.7981592e.chunk.js"
+  },
+  {
+    "revision": "68dd9cb45076a8984821",
+    "url": "/Portfolio/static/js/runtime-main.6106c292.js"
+  },
+  {
+    "revision": "38bbe1dece14bc444a846d7657e5a482",
+    "url": "/Portfolio/static/media/resume.38bbe1de.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.627046ae34531620a73e14ae7038545e.js"
+  "/Portfolio/precache-manifest.33427933f35e14d52954e283b5d82763.js"
 );
 
 self.addEventListener('message', (event) => {
